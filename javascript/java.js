4eafd04ddf46ebcffbcc5a8495e1b6089ba8631f
Georgia <ghobs987@gmail.com>
2021-06-27 23:31:53 +0100

    a bugged interation that works functionally 70% of the time, some question answers have logical errors which stop functionality, but second question is usually presented and repition of questions is prevented.

--- javascript/java.js
+++ javascript/java.js
@@ -28,7 +28,7 @@ var questionarray = [
     }
 
 ];
-var questionsAns = [{choosenQu}
+var questionsAns = [{ choosenQu }
 
 ];
 
@@ -61,7 +61,7 @@ document.getElementById("start").onclick = StartGame;
 function StartGame() {
 
     startEl.hide()
-    
+
 
     timeLeft = 30;
     var timeInterval = setInterval(function () {
@@ -87,146 +87,172 @@ function rdmGen() {
 
 
 
+
 // choose random question
 
 // console.log(rdmGen,)
 
 function question() {
-
+    
     rdmGen()
-    // repititionPrevention()
-    // function repititionPrevention() {
-    //     if(choosenQu === questionsAns){
-    //         rdmGen()
-    //     } else {
-    //     }
-    // }
+    
+    
+    
+    
+    
+    console.log(choosenQu, questionarray[choosenQu]);
+    
+    console.log(questionsAns)
+    
+    var QuPrompt = $('<h1>');
+    QuPrompt.attr("id", "prompt");
+    QuPrompt.text(questionarray[choosenQu].question)
+    rootEl.append(QuPrompt)
+    
+    var btn1El = $('<button>');
+    btn1El.text(questionarray[choosenQu].choice1);
+    // console.log(questionarray[choosenQu].answer)
+    // console.log(questionarray[choosenQu].choice1)
+    btn1El.attr("id", "btn1");
+    btn1El.attr("class", "btn");
+    rootEl.append(btn1El)
+    
+    var btn2El = $('<button>');
+    btn2El.attr("id", "btn2");
+    btn2El.attr("class", "btn");
+    btn2El.text(questionarray[choosenQu].choice2);
+    rootEl.append(btn2El)
+    
+    
+    var btn3El = $('<button>');
+    btn3El.attr("id", "btn3");
+    btn3El.attr("class", "btn");
+    btn3El.text(questionarray[choosenQu].choice3);
+    console.log(questionarray[choosenQu].choice3)
+    rootEl.append(btn3El)
+    
+    
+    
+    
+    var btn4El = $('<button>');
+    btn4El.attr("id", "btn4");
+    btn4El.attr("class", "btn");
+    btn4El.text(questionarray[choosenQu].choice4);
+    console.log(questionarray[choosenQu].choice4)
+    rootEl.append(btn4El)
+    
+    
+    function clear() {
+        
+        QuPrompt.hide()
+        btn1El.hide()
+        btn2El.hide()
+        btn3El.hide()
+        btn4El.hide()
+    }
+    // console.log(questionarray[choosenQu].answer)
+    
+    document.getElementById("btn1").onclick = log
+    
+    function log() {
+        console.log(questionarray[choosenQu].choice1)
+        if (questionarray[choosenQu].choice1 === questionarray[choosenQu].answer) {
+            
+            window.alert("correct")
+            timeLeft = timeLeft + 5;
+            clear()
+            secondQu()
+            
+            
+        }
+        else {
+            window.alert("incorrect")
+            timeLeft = timeLeft - 5;
+            clear()
+            secondQu()
+            
+        }
+    }
+    
+    document.getElementById("btn2").onclick = log2
+    
+    function log2() {
+        console.log(questionarray[choosenQu].choice2)
+        if (questionarray[choosenQu].choice2 === questionarray[choosenQu].answer) {
+            window.alert("correct")
+            timeLeft = timeLeft + 5;
+            clear()
+            secondQu()
+            
+        } else {
+            window.alert("incorrect")
+            timeLeft = timeLeft - 5;
+            clear()
+            secondQu()
+            
+        }
+    }
+    
+    document.getElementById("btn3").onclick = log3
+    
+    function log3() {
+        console.log(questionarray[choosenQu].choice3)
+        if (questionarray[choosenQu].choice3 === questionarray[choosenQu].answer) {
+            window.alert("correct")
+            timeLeft = timeLeft + 5;
+            clear()
+            secondQu()
+            
+            
+        } else {
+            window.alert("incorrect")
+            timeLeft = timeLeft - 5;
+            clear()
+            secondQu()
+        }
+    }
+    
+    document.getElementById("btn4").onclick = log4
+    
+    function log4() {
+        console.log(questionarray[choosenQu].choice4)
+        if (questionarray[choosenQu].choice4 === questionarray[choosenQu].answer) {
+            window.alert("correct")
+            timeLeft = timeLeft + 5;
+            clear()
+            secondQu()
+            
+            
+        } else {
+            window.alert("incorrect")
+            timeLeft = timeLeft - 5;
+            clear()
+            secondQu()
+            
+        }
+    }
+}
+
+
+
+function chooseAnother(questionarray) {
+    questionarray.slice(questionsAns)
+    
+}
+
+function secondQu() {
+    function repititionPrevention() {
+        if (choosenQu === questionsAns) {
+
+            chooseAnother()
+            question()
 
             
-            
-            
-            console.log(choosenQu, questionarray[choosenQu]);
-            
-            console.log(questionsAns)
-            
-            var QuPrompt = $('<h1>');
-            QuPrompt.attr("id", "prompt");
-            QuPrompt.text(questionarray[choosenQu].question)
-            rootEl.append(QuPrompt)
-            
-            var btn1El = $('<button>');
-            btn1El.text(questionarray[choosenQu].choice1);
-            // console.log(questionarray[choosenQu].answer)
-            // console.log(questionarray[choosenQu].choice1)
-            btn1El.attr("id", "btn1");
-            btn1El.attr("class", "btn");
-            rootEl.append(btn1El)
-            
-            var btn2El = $('<button>');
-            btn2El.attr("id", "btn2");
-            btn2El.attr("class", "btn");
-            btn2El.text(questionarray[choosenQu].choice2);
-            rootEl.append(btn2El)
-            
-            
-            var btn3El = $('<button>');
-            btn3El.attr("id", "btn3");
-            btn3El.attr("class", "btn");
-            btn3El.text(questionarray[choosenQu].choice3);
-            console.log(questionarray[choosenQu].choice3)
-            rootEl.append(btn3El)
-            
-            
-            
-            
-            var btn4El = $('<button>');
-            btn4El.attr("id", "btn4");
-            btn4El.attr("class", "btn");
-            btn4El.text(questionarray[choosenQu].choice4);
-            console.log(questionarray[choosenQu].choice4)
-            rootEl.append(btn4El)
-            
-            
-            function clear() {
-                
-                QuPrompt.hide()
-                btn1El.hide()
-                btn2El.hide()
-                btn3El.hide()
-                btn4El.hide()
-            }
-            // console.log(questionarray[choosenQu].answer)
-            
-            document.getElementById("btn1").onclick = log
-            
-            function log() {
-                console.log(questionarray[choosenQu].choice1)
-                if (questionarray[choosenQu].choice1 === questionarray[choosenQu].answer) {
-                    
-                    window.alert("correct")
-                    timeLeft = timeLeft +5; 
-                    clear()
-                    
-                    
-                }
-                else {
-                    window.alert("incorrect")
-                    timeLeft = timeLeft -5; 
-                    clear()
-                    
-                }
-            }
-            
-            document.getElementById("btn2").onclick = log2
-            
-            function log2() {
-                console.log(questionarray[choosenQu].choice2)
-                if (questionarray[choosenQu].choice2 === questionarray[choosenQu].answer) {
-                    window.alert("correct")
-                    clear()
-                    
-                    timeLeft = timeLeft +5; 
-                } else{ 
-                    window.alert("incorrect")
-                    clear()
-                    
-                    timeLeft = timeLeft -5; 
-                }
-            }
-            
-            document.getElementById("btn3").onclick = log3
-            
-            function log3() {
-                console.log(questionarray[choosenQu].choice3)
-                if (questionarray[choosenQu].choice3 === questionarray[choosenQu].answer) {
-                    window.alert("correct")
-                    clear()
-                    
-                    timeLeft = timeLeft +5;
-                    
-                } else {
-                    window.alert("incorrect")
-                    timeLeft = timeLeft -5; 
-                    
-                    question()
-                }
-            }
-            
-            document.getElementById("btn4").onclick = log4
-            
-            function log4() {
-                console.log(questionarray[choosenQu].choice4)
-                if (questionarray[choosenQu].choice4 === questionarray[choosenQu].answer) {
-                    window.alert("correct")
-                    clear()
-                
-      
-            timeLeft = timeLeft +5;
         } else {
-        window.alert("incorrect")
-        clear()
-       
-        timeLeft = timeLeft -5; 
+            question()
+            
+        }
     }
-    }
+    repititionPrevention()
+
 }
